67354126c426d7aca9eded61003acfcf5b7ac33f
alexeynezhdanov <alexeynezhdanov@yandex.ru>
2022-10-11 15:03:48 +0300

    Проектная работа 14

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2),
+    avatar: Joi.string().min(2).url(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
